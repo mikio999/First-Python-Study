0f5ba761694315a33d7a14ddc43785e24a8f9bf2
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-02-05 22:56:19 +0900

    Add files via upload

--- test5.py
+++ test5.py
@@ -37,3 +37,24 @@ print('not', not a>b)
 print('not', not b>a)
 print(not True)
 print(not False)
+
+print('e1 :', 3+12 > 7+3)
+print('e2 :', 5+10*3 > 7+3*20)
+print('e3 :', 5+10 > 3 and 7+3 == 10)
+print('e4 :', 5+10 > 0 and not 7+3 == 10)
+
+score1 =90
+score2 = 'A'
+
+if score1 >= 90 and score2 == 'A' :
+    print('Pass')
+else:
+    print('Fail')
+
+id1 = 'vip'
+id2 = 'admin'
+grade = 'platinum'
+if id1 == 'vip' or id2 == 'admin' :
+    print('관리자 입장')
+if id2 == 'admin' and grade == 'platinum' :
+    print('최상위 관리자')
