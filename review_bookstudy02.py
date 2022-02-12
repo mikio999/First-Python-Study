d2e93c2bce6233b135e70914e13cb5b239223647
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-02-12 22:58:28 +0900

    Add files via upload

---
+++ review_bookstudy02.py
@@ -0,0 +1,36 @@
+# 변수 : snakecase
+# 데이터에 이름을 붙인 것
+pi = 3.14159265
+
+print(pi)
+print()
+number =100
+number += 10
+number += 20
+number += 30
+print(number)
+
+number_01 = 110
+number_01 *= 2
+print(number_01)
+
+string = "안녕하세요"
+string += "!"
+string += "_ 미키오 입니다!"
+print(string)
+print()
+
+#input()함수 : 명령 프롬프트에서 사용자로부터 데이터를 입력받을때 사용하는 함수
+#프롬프트 문자열
+input("인사말을 입력하세요.>")
+string = input("인사말을 입력하세요.>")
+print(string)
+
+print(type(string))
+
+# 입력을 받습니다.
+string = input ("입력>")
+
+# 출력합니다.
+print("자료:", string)
+print("자료형:", type(string))
