802a572b327a49e4a87c0924d488ebf9d9cd3840
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-02-16 19:34:13 +0900

    Add files via upload

--- string function01.py
+++ string function01.py
@@ -41,3 +41,41 @@ print(output_c)
 print("# 빈칸을 0으로 채우기")
 print(output_d)
 print(output_e)
+
+# 기호와 함께 출력하기
+output_f = "{:+d}" .format(52) # 양수
+output_g = "{:+d}" .format(-52) # 음수 
+output_h = "{: d}" .format(52) # 양수: 기호 부분 공백
+output_i = "{: d}" .format(-52) # 음수: 기호 부분 공백
+
+print("# 기호와 함께 출력하기")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
+
+# 조합하기
+output_h = "{:+5d}" .format(52)
+output_i = "{:+5d}" .format(-52)
+output_j = "{:=+5d}" .format(52)
+output_k = "{:=+5d}" .format(-52)
+output_l = "{:+05d}" .format(52)
+output_m = "{:+05d}" .format(-52)
+
+print("#조합하기")
+print(output_h)
+print(output_i)
+print(output_j)
+print(output_k)
+print(output_l)
+print(output_m)
+
+output_a = "{:f}" .format(52.273)
+output_b = "{:15f}" .format(52.273)
+output_c = "{:+15f}" .format(52.273)
+output_d = "{:+015f}" .format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
