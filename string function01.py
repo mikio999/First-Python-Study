48b5dc2b2a50c501d4815f05ce2b2e8a65a07fe1
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-02-20 00:19:19 +0900

    Add files via upload

--- string function01.py
+++ string function01.py
@@ -98,3 +98,17 @@ a.upper()
 print(a.upper())
 a.lower()
 print(a.lower())
+
+print()
+
+input_a = """
+    안녕하세요
+문자열의 함수를 알아봅니다
+"""
+
+print(input_a)
+print(input_a.strip())
+
+print()
+print("TrainA10" .isalnum())
+print("10" .isdigit())
