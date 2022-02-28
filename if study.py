0c1cfadad6ed10fb64b2d5d3f543eb488dac5e7c
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-03-01 01:22:25 +0900

    Add files via upload

--- if study.py
+++ if study.py
@@ -22,3 +22,27 @@ if last_number == 1 \
     or last_number == 7 \
     or last_number == 9:
     print("홀수입니다")
+
+# if문 두 수 비교하기
+A, B = input() .split()
+A = int(A)
+B = int(B)
+if A > B :
+    print('>')
+elif A < B :
+    print('<')
+else :
+    print('==')
+
+# 나머지 연산자를 활용해서 짝수와 홀수 구분
+# 입력을 받습니다.
+number = input("정수 입력> ")
+number = int(number)
+
+# 짝수 조건
+if number % 2 == 0:
+    print("짝수입니다.")
+
+# 홀수 조건
+if number % 2 == 1:
+    print("홀수입니다.")
