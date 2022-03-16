0e2704bf9170cde9b6b1d9215c287ef7277ca4b6
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-03-16 21:51:39 +0900

    Add files via upload

--- chapter05_01.py
+++ chapter05_01.py
@@ -68,3 +68,76 @@ d = func_mul3(30)
 
 print(type(d),d, d.get('v2'), d.items(), d.keys())
 
+# 중요
+# *args, **kwargs
+
+# *args (언팩킹)
+def args_func(*args): # 매개변수 명 자유 *튜플
+    for i, v in enumerate(args):
+        print('Result : {}' .format(i), v)
+    print('-----')
+
+args_func('Lee')
+args_func('Lee', 'Park')
+args_func('Lee', 'Park', 'Kim')
+
+# **kwargs(언팩킹)
+def kwargs_func(**kwargs): # 매개변수 명 자유 **딕셔너리
+    for v in kwargs.keys():
+        print("{}".format(v), kwargs[v])
+    print('-----')
+
+kwargs_func(name1='Lee')
+kwargs_func(name1='Lee', name2='Park')
+kwargs_func(name1='Lee', name2='Park', name3='Cho')
+
+# 전체 혼합
+def example(args_1, args_2, *args, **kwargs):
+    print(args_1, args_2, args, kwargs)
+
+example(10,20,'Lee','Kim','Park', age1=20, age2=30, age3=40)
+
+# 중첩함수
+
+def nested_func(num):
+    def func_in_func(num):
+        print(num)
+    print("In func")
+    func_in_func(num+100)
+
+nested_func(100)
+
+# 실행불가
+# func_in_func(1000)
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스(메모리) 할당
+# 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
+# 남발 시 가독성 오히려 감소
+
+
+#def mul_func(x, y):
+#    return x*y
+
+#a = lambda x, y:x*y
+
+# 일반적 함수 -> 할당
+def mul_func(x,y):
+    return x*y
+
+print(mul_func(10,50))
+
+mul_func_var = mul_func
+print(mul_func_var(20,50))
+
+# 람다 함수 -> 할당
+lambda_mul_func = lambda x,y:x*y
+print(lambda_mul_func(50,50))
+
+def func_final(x, y, func):
+    print('>>>>',x*y*func(100, 100))
+
+
+func_final(10, 20, mul_func_var)
+func_final(10, 20, lambda x,y:x*y)
