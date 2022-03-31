24dae24cc78120e1337efcdf2d2a1e81de1566b3
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-03-31 22:43:44 +0900

    Add files via upload

---
+++ 오픈소스 SW와 파이썬 프로그래밍/함수/Lab:생일축하함수.py
@@ -0,0 +1,14 @@
+# 생일 축하 메시지를 출력하는 함수 happyBirthday()를 작성해보자.
+def happyBirthday():
+    print("생일축하 합니다!\n"*2)
+    print("사랑하는 친구의", end= "")
+    print("생일축하 합니다!")
+happyBirthday()
+
+print()
+def Birthday():
+    print("생일축하 합니다!")
+    print("생일축하 합니다!")
+    print("사랑하는 친구의", end= " ")
+    print("생일축하 합니다!")
+Birthday()
