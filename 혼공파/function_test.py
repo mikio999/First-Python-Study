fb658ab38353bd045841a35d22d94825c5213e9e
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-04-08 01:51:01 +0900

    Add files via upload

--- 혼공파/function_test.py
+++ 혼공파/function_test.py
@@ -1,4 +1,13 @@
 # 리스트 평탄화 
 def flatten(data):
-    output = 0
+    output = []
+    for item in data:
+        if type(item) == list:
+            output += flatten(item)
+        else:
+            output.append(item)
+    return output
+example = [[1,2,3],[4,[5,6]],7,[8,9]]
+print("원본:", example)
+print("변환:", flatten(example))
     
