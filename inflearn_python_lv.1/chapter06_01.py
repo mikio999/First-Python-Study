e7d67ab27c227983864b0ca716ea828c64f70802
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-04-10 22:45:50 +0900

    Add files via upload

--- inflearn_python_lv.1/chapter06_01.py
+++ inflearn_python_lv.1/chapter06_01.py
@@ -8,14 +8,17 @@
 # 인스턴스 변수: 객체마다 별도 존재
 
 # 예제1
+from unicodedata import name
+
+
 class Dog: # object 상속
     # 클래스 속성
     species = 'firstdog'
 
     # 초기화/인스턴스 속성
     def __init__(self, name, age) :
-        self.name = name
-        self.age = age
+        self.name = name # self.a = name
+        self.age = age # self.b = age
 
 # 클래스 정보
 print(Dog)
@@ -40,3 +43,53 @@ if a.species == 'firstdog' :
 
 print(Dog.species)
 print(a.species)
+
+# 예제2
+# self의 이해
+class SelfTest:
+    def func1():
+        print('Func1 called')
+    def func2(self):
+        print(id(self))
+        print('Func2 called')
+
+f = SelfTest()
+
+print(dir(f))
+
+# print(dir(f))
+print(id(f))
+# f.func1() # 예외
+f.func2()
+SelfTest.func1()
+# SelfTest.func2() # 예외
+SelfTest.func2(f)
+
+# 예제3
+# 클래스 변수, 인스턴스 변수
+class Warehouse: 
+    # 클래스 변수
+    stock_num = 0 # 재고
+
+    def __init__(self, name):
+        # 인스턴스 변수
+        self.name = name
+        Warehouse.stock_num += 1
+
+    def __del__(self):
+        Warehouse.stock_num -= 1
+
+user1 = Warehouse('Lee')
+user2 = Warehouse('Cho')
+
+print(Warehouse.stock_num)
+# Warehouse.stock_num = 50
+print(user1.name)
+print(user2.name)
+print(user1.__dict__)
+print(user2.__dict__)
+print(Warehouse.__dict__)
+print('>>>>',user1.stock_num)
+
+del user1
+print('after', Warehouse.__dict__)
