6a1b71fbdd3f8f7ebbd6cf8441b8d0b6468a6fff
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-04-14 22:27:17 +0900

    Add files via upload

--- 오픈소스 SW와 파이썬 프로그래밍/list/Lab:누적값_리스트.py
+++ 오픈소스 SW와 파이썬 프로그래밍/list/Lab:누적값_리스트.py
@@ -2,3 +2,6 @@ list1 = [10,20,30,40,50]
 list2 = [sum(list1[0:x+1]) for x in range(0,len(list1))]
 print(list1)
 print(list2)
+
+list_test = ['a','b','c','d','e']
+list_for = [sum(list_test) for t in list_test]
