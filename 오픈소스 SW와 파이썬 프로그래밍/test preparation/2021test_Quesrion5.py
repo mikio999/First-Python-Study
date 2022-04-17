d628ec0c29fb5b7b736e8529c69fdcf13c02dd07
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-04-17 21:49:36 +0900

    Add files via upload

---
+++ 오픈소스 SW와 파이썬 프로그래밍/test preparation/2021test_Quesrion5.py
@@ -0,0 +1,38 @@
+#[Question-5] 당신의 로봇 생산 공장에서 6대의 로봇을 생산하였다. 당신은 그 로봇들 각각에 일련번호와
+# 이름을 부여하기를 원한다. 일련번호는 100, 101, 102, ... 과 같이 100부터 시작하여 1씩 증가한 숫자를
+# 부여한다. 일련번호가 홀수이면 여성 이름들 중에서, 짝수이면 남성 이름들 중에서 랜덤하게 선택하여, 그
+# 로봇에게 이름을 부여한다. 여성 이름들과 남자 이름들의 후보들은 다음과 같이 리스트로 각각 주어져
+# 있다. 단, 로봇이름은 중복됨이 없이 서로 다르게 부여한다.
+
+female_names = ["Alice", "Bianca", "Cindy", "Dorothy", "Emily", "Hera", "Isabel", "Jane","Julia", "Lisa", "Monica"]
+male_names = ["Alex", "Bradley", "Charles", "Eric", "George", "Harry", "Ivan", "James", "Kevin", "Lucas", "Mickey", "Sam"]
+
+key1 =[]
+for i in range(100,106,2):
+    key1.append(i)
+print(key1)
+
+key2 = []
+for i in range(101,107,2):
+    key2.append(i)
+print(key2)
+
+import random
+f_list = random.sample(female_names,3)
+m_list = random.sample(male_names, 3)
+print(f_list)
+print(m_list)
+
+f_name = dict(zip(f_list, key1))
+m_name = dict(zip(m_list, key2))
+
+print(f_name)
+print(m_name)
+
+f_name.update(m_name)
+print(f_name)
+final_name = sorted(f_name.items())
+print(final_name)
+
+for name in final_name:
+    print(name)
