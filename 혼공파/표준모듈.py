2d17dbf060cb5bf24e229ed05ccea96189ada674
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-04-22 16:11:18 +0900

    Add files via upload

---
+++ 혼공파/표준모듈.py
@@ -0,0 +1,14 @@
+# module 모듈 : 파이썬은 모듈을 활용해 코드를 분리하고 공유
+# 모듈은 여러 변수와 함수를 까지고 있는 집합체로 표준 모듈과 외부 모듈로 구분.
+# 표준 모듈: 파이썬에 기본적으로 내장되어 있는 모듈
+# 외부 모듈: 다른 사람들이 만들어서 꽁개한 모듈
+# import 모듈 이름
+
+# 모듈 사용의 기본: math 모듈
+import math
+print(math.sin(1))
+print(math.cos(1))
+print(math.tan(1))
+print(math.floor(2.5)) # 내림
+print(math.ceil(2.5)) # 올림
+
