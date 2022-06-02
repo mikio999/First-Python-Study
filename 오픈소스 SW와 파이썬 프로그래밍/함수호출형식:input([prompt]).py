c9a81553abaa3b084f23535df60eae5304881f33
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-06-02 21:22:46 +0900

    Add files via upload

---
+++ 오픈소스 SW와 파이썬 프로그래밍/함수호출형식:input([prompt]).py
@@ -0,0 +1,7 @@
+age = int(input('당신의 나이를 입력하십시오:'))
+name, age = input('Enter your name and age :').split()
+
+num1, num2, num3 = input("숫자 3개를 입력하시오:").split()
+num1, num2, num3 = int(num1), int(num2), int(num3)
+
+num1, num2, num3 = map(int, input('숫자 3개를 입력하십시오: ').split())
