94b4040a473c28e76e02684eb81aeb4b05c3076b
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-06-17 22:20:53 +0900

    Add files via upload

---
+++ 오픈소스 SW와 파이썬 프로그래밍/NumPy/데이터_생성_함수.py
@@ -0,0 +1,17 @@
+import numpy as np
+A = np.arange(1,10,2)
+print(A)
+
+import matplotlib.pyplot as plt
+print(plt.plot(A))
+plt.show()
+
+print(np.linspace(0,10,5))
+print(np.linspace(0,10,4))
+
+A =np.linspace(0,10,100)
+print(A)
+
+import matplotlib.pyplot as plt
+print(plt.plot(A))
+plt.show
