0c689b470c0d61058b3fcd493aa33d9ee1f119bd
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-06-20 21:59:21 +0900

    Add files via upload

--- inflearn_python_lv.1/chapter06_01.py
+++ inflearn_python_lv.1/chapter06_01.py
@@ -93,3 +93,29 @@ print('>>>>',user1.stock_num)
 
 del user1
 print('after', Warehouse.__dict__)
+
+# 예제4
+class Dog: # object 상속
+    # 클래스 속성
+    species = 'firstdog'
+
+    # 초기화/ 인스턴스 속성
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def info(self):
+        return '{} is {} years old'.format(self.name, self.age)
+
+    def speak(self,sound):
+        return "{} says {}!".format(self.name, sound)
+
+# 인스턴스 생성
+c = Dog('july', 4)
+d = Dog('Marry', 10)
+# 메소드 호출
+print(c.info())
+print(d.info())
+# 메소드 호출
+print(c.speak('Wal Wal'))
+print(d.speak('Mung Mung'))
