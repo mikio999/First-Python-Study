c31fc843ef327f476634deef83c7dd53aaba86bb
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-06-22 23:28:18 +0900

    Add files via upload

--- inflearn_python_lv.1/chapter06_02.py
+++ inflearn_python_lv.1/chapter06_02.py
@@ -18,11 +18,22 @@ def power(x, y):
     return x**y
 
 
-print('-' * 15)
-print('called! inner!')
-print(add(5,5))
-print(subtract(15,5))
-print(multiply(5,5))
-print(divide(10,2))
-print(power(5,3))
-print('-' * 15)
+# print('-' * 15)
+# print('called! inner!')
+# print(add(5,5))
+# print(subtract(15,5))
+# print(multiply(5,5))
+# print(divide(10,2))
+# print(power(5,3))
+# print('-' * 15)
+
+# __name__ 사용
+if __name__ == "__main__":
+    print('-' * 15)
+    print('called! inner!')
+    print(add(5,5))
+    print(subtract(15,5))
+    print(multiply(5,5))
+    print(divide(10,2))
+    print(power(5,3))
+    print('-' * 15)
--- inflearn_python_lv.1/module_test.py
+++ inflearn_python_lv.1/module_test.py
@@ -2,5 +2,20 @@
 
 import sys
 
-print(sys)
 print(sys.path)
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+# sys.path.append('/users/')
+
+print(sys.path)
+
+# import test_module
+
+# 모듈 사용
+# print(test_module.power(10,3))
+
+import chapter06_02
+
+print(chapter06_02.add(10,10000))
