4fa74e0c62f447c69003ab238183c17092edbb34
mikio999 <96870855+mikio999@users.noreply.github.com>
2022-06-30 23:15:26 +0900

    Add files via upload

--- inflearn_python_lv.1/chapter09_01.py
+++ inflearn_python_lv.1/chapter09_01.py
@@ -69,3 +69,23 @@ with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
 print()
 
 # 파일 쓰기 (write)
+
+# 예제1
+with open('./resource/contents1.txt','w') as f:
+    f.write('I love python\n')
+
+# 예제2
+with open('./resource/contents1.txt','at') as f:
+    f.write('I love python2\n')
+
+# 예제3
+# writelines: 리스트 -> 파일
+with open('./resource/contents1.txt','w') as f:
+    list = ['Orange\n', 'Apple\n', 'Banana\n', 'Melon\n']
+    f.writelines(list)
+
+# 예제4
+with open('./resource/contents1.txt','w') as f:
+    print('Test Text Write!', file=f)
+    print('Test Text Write!', file=f)
+    print('Test Text Write!', file=f)
